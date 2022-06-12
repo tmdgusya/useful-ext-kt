f55fea03ffcd9efde909815bbe8c8bd1e7565b19
tmdgusya <tmdgusya@naver.com>
2022-06-12 17:28:25 +0900

    [#1] add Detekt

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
+
+plugins {
+    id("org.jetbrains.kotlin.jvm").version("1.6.10")
+    id("io.gitlab.arturbosch.detekt").version("1.20.0")
+}
+
+repositories {
+    mavenCentral()
+}
+
+detekt {
+    buildUponDefaultConfig = true
+    allRules = false
+    config = files("$projectDir/detekt/detekt.yml")
+    baseline = file("$projectDir/detekt/baseline.yml")
+}
+
+tasks.withType<Detekt>().configureEach {
+    reports {
+        html.required.set(true) // observe findings in your browser with structure and code snippets
+        xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+        txt.required.set(true) // similar to the console output, contains issue signature to manually edit baseline files
+        sarif.required.set(true) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with Github Code Scanning
+    }
+}
+
+tasks.withType<Detekt>().configureEach {
+    jvmTarget = "11"
+}
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+    jvmTarget = "11"
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+}
