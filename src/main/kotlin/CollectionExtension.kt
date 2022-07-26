c46ab1b39bf30fa7b6c398e3fdc224a4a8150d77
tmdgusya <tmdgusya@naver.com>
2022-07-26 17:50:07 +0900

    [feat] add parallelExecuteWithChunk

--- src/main/kotlin/CollectionExtension.kt
+++ src/main/kotlin/CollectionExtension.kt
@@ -1,7 +1,35 @@
+import kotlinx.coroutines.CoroutineStart
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.coroutines.CoroutineContext
+
 /**
  * I think `!in` is not useful (it's just my opinion)
  * So i created notContains method
  */
 infix fun <T> Collection<T>.notContains(ele: T): Boolean {
     return !this.contains(ele)
+}
+
+/**
+ * @chunkSize Splits this collection into a list of lists each not exceeding the given size.
+ * @coroutineContext Inject Coroutine Context which you want use it
+ * @supplier Inject Supplier that is want to execute work
+ *
+ */
+fun <I, O> Collection<I>.parallelExecuteWithChunk(
+    chunkSize: Int,
+    coroutineContext: CoroutineContext,
+    supplier: (Collection<I>) -> O,
+    coroutineStartMethod: CoroutineStart = CoroutineStart.LAZY
+) = runBlocking {
+    return@runBlocking this@parallelExecuteWithChunk.chunked(chunkSize).map { chunkedIds ->
+        async(start = coroutineStartMethod) {
+            withContext(context = coroutineContext) {
+                supplier.invoke(chunkedIds)
+            }
+        }
+    }.awaitAll()
 }
