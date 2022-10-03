7969aafab3b295e6c4443484577fa44a4e3dbd65
tmdgusya <tmdgusya@naver.com>
2022-10-03 17:11:37 +0900

    [FIX] using contract feature in isNull And isNotNull methods for the smartcast

--- src/main/kotlin/AnyExtension.kt
+++ src/main/kotlin/AnyExtension.kt
@@ -1,13 +1,24 @@
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.contract
+
 /**
  * @return The return value is a Boolean value that determines whether it is null or not.
  */
+@OptIn(ExperimentalContracts::class)
 fun Any?.isNull(): Boolean {
+    contract {
+        returns(true) implies (this@isNull == null)
+    }
     return this == null
 }
 
 /**
  * @return The return value is a Boolean value that determines whether it is not null or not.
  */
+@OptIn(ExperimentalContracts::class)
 fun Any?.isNotNull(): Boolean {
+    contract {
+        returns(true) implies (this@isNotNull != null)
+    }
     return this != null
 }
